5fa4bfb1bf8b895e44163753c1dabaa4d9a1cfd3
Michał Chraniuk <foxbond@o2.pl>
2018-10-05 13:23:10 +0200

    Add admin/createUser

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bcrypt = require('bcrypt-nodejs');
+
 var router = express.Router();
 
 router.all('*', function(req, res, next){
@@ -18,8 +20,45 @@ router.get('/', function(req, res) {
 	res.render('admin/index');
 });
 
-router.get('/', function(req, res, next) {
-	res.render('admin/index');
+router.get('/createUser', function(req, res) {
+	res.render('admin/createUser');
 });
+
+router.post('/createUser', function(req, res, next) {
+	
+	if (!req.body.userMail || !req.body.userName || !req.body.userPassword){
+		return res.render('admin/createUser', { 
+				message: 'Fill all fields!', 
+				_form: {
+					mail: req.body.userMail,
+					name: req.body.userName,
+					password: req.body.userPassword
+				}
+			});
+	}
+	
+	//TODO: Maybe sanitize input?
+	var userMail = req.body.userMail.trim();
+	var userName = req.body.userName.trim();
+	var userPassword = req.body.userPassword.trim();
+	
+	bcrypt.hash(userPassword, null, null, function(err, hash) {
+		if (err){
+			log.error('Unable to compute hash! ("'+err+'")');
+			return next(createError(500)); 
+		}
+		
+		db.query('INSERT INTO users VALUES (NULL, ?, ?, ?)', [userMail, userName, hash], function(err){
+			if (err){
+				log.error('DB Query error! ("'+err+'")');
+				return next(createError(500)); 
+			}
+			
+			res.render('admin/createUser', {
+				message: 'User "'+userName+'" added!'
+			});
+		});
+	});
+});//router.post('/createUser'
 
 module.exports = router;
