1612178a19ef2b60dc8b94663418b04ae0e73801
Michał Chraniuk <foxbond@o2.pl>
2018-10-09 14:58:59 +0200

    Add "generateNav" helper

---
+++ tests/helper_generateNav.js
@@ -0,0 +1,12 @@
+﻿var generateNav = require('../public/js/helpers').helpers.generateNav;
+
+var url = '/admin';
+var url2 = '/admin/listusers';
+var url3 = '/admin/listusers/modify';
+
+console.log(url);
+console.log(generateNav(url));
+console.log(url2);
+console.log(generateNav(url2));
+console.log(url3);
+console.log(generateNav(url3));
