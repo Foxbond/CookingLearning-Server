6c6810781e82aeaa59e46012103beeb63f68a72f
Michał Chraniuk <foxbond@o2.pl>
2018-10-09 12:22:32 +0200

    Update db structure

--- db.sql
+++ db.sql
@@ -1,10 +1,27 @@
+CREATE TABLE `groups` (
+  `groupId` int(11) NOT NULL,
+  `groupName` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `usergroups` (
+  `userId` int(11) NOT NULL,
+  `groupId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `users` (
   `userId` int(11) NOT NULL,
-  `userMail` varchar(64) NOT NULL,
+  `userMail` varchar(255) NOT NULL,
   `userName` varchar(20) NOT NULL,
   `userPassword` char(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`groupId`);
+
+ALTER TABLE `usergroups`
+  ADD KEY `userId` (`userId`),
+  ADD KEY `groupId` (`groupId`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userId`),
@@ -12,5 +29,11 @@ ALTER TABLE `users`
   ADD KEY `userName` (`userName`);
 
 
+ALTER TABLE `groups`
+  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+ALTER TABLE `usergroups`
+  ADD CONSTRAINT `groupId` FOREIGN KEY (`groupId`) REFERENCES `groups` (`groupId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE;
