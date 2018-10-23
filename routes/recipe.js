7f9c7e7021bc0978ed6a8d4de6d0fd51400c8aef
Michał Chraniuk <foxbond@o2.pl>
2018-10-23 10:50:34 +0200

    Redirect if user provide correct recipeId but wrong url

--- routes/recipe.js
+++ routes/recipe.js
@@ -46,14 +46,23 @@ router.get('/:recipeId(\\d+)/:recipeName', function route_showFull(req, res, nex
 		return next();
 	}
 
-	/**
-	 * TODO: Redirect on correct id but wrong name
-	 * res.status(301).location('/recipe/:recipeId/:recipeName');
-	 * res.status(404).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/recipe/:recipeId/:recipeName"></head></html>');
-	 */
+	db.query('SELECT recipeUrl FROM recipes WHERE recipeId=?', [recipeId], function db_fetchRecipe(err, data) {
+		if (err) {
+			return next(err);
+		}
 
-	res.send('/recipe/:recipeId/:recipeName');
-});
+		if (data.length != 1) {
+			return next();
+		}
+
+		if (req.params.recipeName != data[0].recipeUrl) {
+			//Maybe it's better to redirect this way? res.status(404).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/recipe/:recipeId/:recipeName"></head></html>');
+			return res.status(301).location('/recipe/' + recipeId + '/' + data[0].recipeUrl);
+		}
+
+		res.send('/recipe/:recipeId/:recipeName');
+	});
+});//router.get('/:recipeId(\\d+)/:recipeName
 
 
 router.get('/:recipeId(\\d+)*', function route_showError(req, res) {
@@ -61,7 +70,7 @@ router.get('/:recipeId(\\d+)*', function route_showError(req, res) {
 
 	res.status(404);
 	res.send('/recipe/:recipeId error');
-});
+});//router.get('/:recipeId(\\d+)*
 
 router.get('/browse', function route_browse(req, res) {
 	res.render('recipe/browse');
